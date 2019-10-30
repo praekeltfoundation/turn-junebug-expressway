b2ad6845ec0346c7e702c00983ddb237d34bd0d2
Erik Harding <erik@praekelt.org>
2019-10-30 17:14:49 +0200

    test message on queue

--- test/turn_junebug_expressway_web/controllers/message_controller_test.exs
+++ test/turn_junebug_expressway_web/controllers/message_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule TurnJunebugExpresswayWeb.MessageControllerTest do
   use TurnJunebugExpresswayWeb.ConnCase
 
+  alias TurnJunebugExpresswayWeb.Utils
+
   describe "validate hmac signature in header" do
     test "error when hmac header is invalid", %{} do
       conn =
@@ -31,6 +33,42 @@ defmodule TurnJunebugExpresswayWeb.MessageControllerTest do
           "type" => "text"
         })
 
+      {:ok, connection} = AMQP.Connection.open(Utils.get_env(:rabbitmq, :urn))
+      {:ok, channel} = AMQP.Channel.open(connection)
+
+      queue_name = Utils.get_env(:rabbitmq, :messages_queue)
+
+      AMQP.Queue.subscribe(channel, "#{queue_name}.outbound", fn payload, _meta ->
+        {:ok,
+         %{
+           "content" => content,
+           "from_addr" => from_addr,
+           "in_reply_to" => in_reply_to,
+           "message_id" => message_id,
+           "message_type" => message_type,
+           "message_version" => message_version,
+           "session_event" => session_event,
+           "timestamp" => timestamp,
+           "to_addr" => to_addr,
+           "transport_metadata" => transport_metadata,
+           "transport_name" => transport_name,
+           "transport_type" => transport_type
+         }} = Jason.decode(payload)
+
+        assert content == "text message content"
+        assert from_addr == "+2712345"
+        assert in_reply_to == nil
+        assert message_id != nil
+        assert message_type == "user_message"
+        assert message_version == "20110921"
+        assert session_event == nil
+        assert timestamp != nil
+        assert to_addr == "whatsapp_id"
+        assert transport_metadata == %{}
+        assert transport_name == "dummy_messages_queue"
+        assert transport_type == "telnet"
+      end)
+
       conn =
         build_conn()
         |> put_req_header(
@@ -44,6 +82,8 @@ defmodule TurnJunebugExpresswayWeb.MessageControllerTest do
       {:ok, %{"messages" => [%{"id" => message_id}]}} = Jason.decode(conn.resp_body)
 
       assert message_id != nil
+
+      AMQP.Connection.close(connection)
     end
   end
 end
