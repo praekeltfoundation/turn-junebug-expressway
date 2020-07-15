e6abd0f7a6ac3f8d1254309eaa4661129996ca2f
Erik Harding <erik@praekelt.org>
2020-07-15 15:54:36 +0200

    fix duplicate code

--- lib/turn_junebug_expressway/turn_client.ex
+++ lib/turn_junebug_expressway/turn_client.ex
@@ -24,36 +24,15 @@ defmodule TurnJunebugExpressway.TurnClient do
 
   def post_event(client, body) do
     client
-    |> do_post(Utils.get_env(:turn, :event_path), body)
+    |> do_post(Utils.get_env(:turn, :event_path), body, [{"x-turn-fallback-channel", "1"}])
   end
 
   def post_inbound(client, body) do
     client
     |> do_post(Utils.get_env(:turn, :inbound_path), body, [
+      {"x-turn-fallback-channel", "1"},
       {"authorization", "Bearer " <> Utils.get_env(:turn, :token)},
       {"accept", "application/vnd.v1+json"}
     ])
-  end
-
-  def do_post(client, path, body, headers \\ []) do
-    case client
-         |> post(path, body,
-           headers: [
-             {"x-turn-fallback-channel", "1"} | headers
-           ]
-         ) do
-      {:ok, %Tesla.Env{status: status}}
-      when status in 200..299 ->
-        :ok
-
-      {:ok, %Tesla.Env{status: status} = reason} ->
-        {:error, status, reason}
-
-      {:error, %Tesla.Env{status: status} = reason} ->
-        {:error, status, reason}
-
-      {:error, reason} when is_atom(reason) ->
-        {:error, 503, reason}
-    end
   end
 end
