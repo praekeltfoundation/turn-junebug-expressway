c4ec9a0f34647be4155a87e8cff3af7bdbcbfee2
Matthew <wppmat001@myuct.ac.za>
2024-08-08 11:10:51 +0200

    Renaming agent.ex and adding comments

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 import Config
 
-config :turn_junebug_expressway, :agent, ttl: System.get_env("MESSAGE_TTL", "10000")
+config :turn_junebug_expressway, :agent, ttl: System.get_env("RECIPIENT_ID_TTL", "10000")
 
 # Configures the endpoint
 config :turn_junebug_expressway, TurnJunebugExpresswayWeb.Endpoint,
